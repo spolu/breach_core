184b69feb5e3b1c39bb25551b8c8cdff81d6a9e0
Stanislas Polu <polu.stanislas@gmail.com>
2014-05-15 14:26:15 -0700

    Transient version to bootstrap auto-update

--- dist/linux_wrapper.sh
+++ dist/linux_wrapper.sh
@@ -2,4 +2,6 @@
 
 export SRC_DIR=$(cd "$(dirname "$0")"; pwd)
 
-AUTO_UPDATE=true $SRC_DIR/__AUTO_UPDATE_BUNDLE__/exo_browser/exo_browser --raw $SRC_DIR/__AUTO_UPDATE_BUNDLE__/breach_core --expose-gc
+BREACH_AUTO_UPDATE=true                                         \
+  $SRC_DIR/__AUTO_UPDATE_BUNDLE__/exo_browser/exo_browser       \
+  --raw $SRC_DIR/__AUTO_UPDATE_BUNDLE__/breach_core --expose-gc
