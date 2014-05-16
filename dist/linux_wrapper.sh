9bf013f537ed84103476530284acb73442a6548c
Stanislas Polu <stan@teleportd.com>
2014-05-16 16:14:07 -0700

    Invereted env to control AUTO_UDPATE

--- dist/linux_wrapper.sh
+++ dist/linux_wrapper.sh
@@ -2,6 +2,5 @@
 
 export SRC_DIR=$(cd "$(dirname "$0")"; pwd)
 
-BREACH_AUTO_UPDATE=true                                         \
-  $SRC_DIR/__AUTO_UPDATE_BUNDLE__/exo_browser/exo_browser       \
+$SRC_DIR/__AUTO_UPDATE_BUNDLE__/exo_browser/exo_browser         \
   --raw $SRC_DIR/__AUTO_UPDATE_BUNDLE__/breach_core --expose-gc
